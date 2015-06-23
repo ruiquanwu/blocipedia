e6b2b240447688f6dc94ac1b27d2362f02c463c7
ruiquan wu <ruiquanwu@gmail.com>
2015-06-23 19:32:47 +0000

    completed seed-data

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'figaro', '1.0'
 
 gem 'pundit'
 
+gem 'faker'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new(
+  name: "Admin User",
+  email: "admin@example.com",
+  password: "helloworld",
+  role: "admin"
+  )
+admin.skip_confirmation!
+admin.save!
+
+premium = User.new(
+  name: "Premium User",
+  email: "premium@gmail.com",
+  password: "helloworld",
+  role: "premium"
+  )
+premium.skip_confirmation!
+premium.save!
+
+standard = User.new(
+  name: "Member",
+  email: "member@example.com",
+  password: "helloworld",
+  )
+standard.skip_confirmation!
+standard.save!
+
+users = User.all
+
+10.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    user: users.sample
+    )
+end
+  
